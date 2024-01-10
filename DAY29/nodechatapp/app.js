c6972368499b211fbcae85291f14fdeb2671957f
minjun <rhalswns098@gmail.com>
2024-01-10 17:08:58 +0900

    DAY30

--- DAY29/nodechatapp/app.js
+++ DAY29/nodechatapp/app.js
@@ -3,7 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+require('dotenv').config();
+
+// CORS 접근 이슈 해결을 위한 cors패키지 참조
+const cors = require("cors");
+
+var sequelize = require('./models/index.js').sequelize;
+
+
 var expressLayouts = require('express-ejs-layouts');
+
 
 // 웹페이지 요청과 응답처리 전용 라우터파일 참조
 var indexRouter = require('./routes/index');
@@ -17,6 +27,21 @@ var memberAPIRouter = require('./routes/memberAPI');
 
 
 var app = express();
+
+//mysql과 자동연결처리 및 모델기반 물리 테이블 생성처리제공
+sequelize.sync();
+
+// 모든 RESTful 호출에 대한 응답 허락하기
+// app.use(cors());
+
+// 특정 도메인에만 허락하기
+app.use(
+  cors({
+    methods: ["GET", "POST", "DELETE", "OPTIONS"],
+    origin: ["http://localhost:3005", "http://naver.com"]
+  })
+);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- DAY29/nodechatapp/routes/index.js
+++ DAY29/nodechatapp/routes/index.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+// DB객체 참조하기
+var db = require('../models/index');
+
+var bcrypt = require('bcryptjs');
+
+/*
+우리가 개발하는 2가지 방식
+1) 100% 서버측 기술사용(Server side) : MVC 패턴 FORM기반 화면 및 데이터 처리 방식 - NO RESTful 방식 > 관리자 웹사이트
+2) 프론트엔드(AJAX)-백엔드(RESTful)기반으로 개발하는 방식 
+2.1 - 사용자 웹사이트 프론트엔드(AJAX)-백엔드(RESTful)기반 개발방식-풀스택
+
+2.2 - 사용자 웹사이트도 100% 서버사이드 기술로 개발이 가능
+*/
 
 /* 임시메인 */
 router.get('/', function(req, res, next) {
@@ -16,14 +29,32 @@ router.get('/entry', function(req, res, next) {
 
 
 /* 회원가입 웹페이지 요청과 응답 */
-router.post('/entry', function(req, res, next) {
+router.post('/entry', async(req, res, next) => {
 
   // step1 : 회원가입 페이지에서 사용자가 입력한 외원정보 추출
   var email = req.body.email;
   var password = req.body.password;
+  var name = req.body.name;
 
+
+  // 사용자 암호 단방향 암호화 적용
+  var ecryptedPassword = await bcrypt.hash(password, 12);
 
   // DB신규 회원등록처리
+  var member = {
+    email,
+    member_password:ecryptedPassword,
+    name,
+    profile_img_path:"",
+    telephone:"",
+    entry_type_code:0,
+    use_state_code:1,
+    birth_date:111111,
+    reg_date:Date.now(),
+    reg_member_id:0
+  };
+
+  await db.Member.create(member);
 
 
 
@@ -39,8 +70,32 @@ router.get('/login', function(req, res, next) {
 
 
 /* 로그인 웹페이지 요청과 응답 */
-router.post('/login', function(req, res, next) {
-  res.redirect('/chat');
+router.post('/login', async(req, res, next) => {
+  var email = req.body.email;
+  var password = req.body.password;
+
+  var member = await db.Member.findOne({ where:{ email } });
+
+  var resultMsg = "";
+
+
+  if(!member){
+    // email 틀림
+    resultMsg = "동일한 메일주소가 존재하지 않습니다.";
+    res.render('login', {resultMsg});
+
+  }else{
+
+    var result = await bcrypt.compare(password, member.member_password);
+
+    if(result){
+      res.redirect('/');
+    }else{
+      resultMsg = "사용자 암호가 일치하지 않습니다.";
+      res.render('login', {resultMsg})
+
+    }
+  }
 });
 
 
